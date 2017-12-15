092a696bc029efb81d04849270c835ed65fd6f33
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2017-12-15 22:35:57 +0100

    line_item validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,14 @@
 class LineItem < ApplicationRecord
-  belongs_to :product, presence: true
+  belongs_to :product
   belongs_to :order
+
+  validates_presence_of :product_id
+  validates :quantity, numericality: { greater_than: 0 }
+  validate :net_price_of_product
+
+  def net_price_of_product
+    if self.net_price != product&.net_price
+      errors.add(:net_price, "net_price must equal product net_price")
+    end
+ end
 end
