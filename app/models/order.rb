5f02b60110574bb1043a5788dc123f023c32b897
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2017-12-15 22:36:21 +0100

    order validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
   has_many :line_items
+
+  before_create { self.order_date = Date.today unless order_date.present }
+
+  validate :order_date_cannot_be_in_the_past
+
+  def order_date_cannot_be_in_the_past
+   if order_date.present? && order_date < Date.today
+     errors.add(:order_date, "can't be in the past")
+   end
+ end
 end
