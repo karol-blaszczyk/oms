f4d68e33646cc88ebea9d335944c53b290dee8d3
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2017-12-16 18:42:29 +0100

    uniq name index to product

--- db/migrate/20171215214424_add_uniq_index_to_products.rb
+++ db/migrate/20171215214424_add_uniq_index_to_products.rb
@@ -1,5 +1,6 @@
 class AddUniqIndexToProducts < ActiveRecord::Migration[5.1]
   def change
     add_index :products, [:name, :net_price], unique: true
+    add_index :products, :name, unique: true
   end
 end
