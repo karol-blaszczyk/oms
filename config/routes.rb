501f7ff8e8783148239e80d303bb7c450284f269
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2017-12-16 18:43:32 +0100

    product ar name uniqness validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :update] do
-    namespace :orders do
-      resources :line_items
-      resource :status_transactions, only: [:show, :create]
-    end
+    resources :line_items
+    resource :status_transactions, only: [:show, :create]
   end
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
