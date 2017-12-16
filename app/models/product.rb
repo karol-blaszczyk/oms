668c83a9169a40842621d3b0971c791861dcefa6
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2017-12-16 18:43:32 +0100

    product ar name uniqness validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@ class Product < ApplicationRecord
   has_many :orders, through: :line_items, dependent: :restrict_with_error
 
   validates :name, presence: true
+  validates_uniqueness_of :name
   validates :net_price, numericality: { greater_than: 0 }
 end
